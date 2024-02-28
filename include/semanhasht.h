f8bed882bd2cc97850b7e39608feb5ccb00e859e
AM205379 <am205379@gmail.com>
2024-02-28 23:31:05 +0330

    Cost constructor is defined. it reads Cost List.txt and int property

--- include/semanhasht.h
+++ include/semanhasht.h
@@ -29,3 +29,53 @@ private:
 };
 
 #endif // SEMANHASHT_H
+/*
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+struct Path {
+    int Start, End, Length, TP;
+    Path() : Start(0), End(0), Length(0), TP(0) {}
+    Path(const string& filename) {
+        readAndStoreData(filename);
+    }
+    void readAndStoreData(const string& filename) {
+        ifstream file(filename);
+        vector<vector<Path>> Abass;
+    if (file.is_open()) {
+        string line;
+        while (getline(file, line)) {
+            istringstream iss(line);
+            string token;
+            vector<Path> currentPaths;
+            while (getline(iss, token, '$')) {
+                istringstream pathStream(token);
+                Path path;
+                pathStream >> path.Start;
+                pathStream.ignore();
+                pathStream >> path.End;
+                pathStream.ignore();
+                pathStream >> path.Length;
+                pathStream.ignore();
+                pathStream >> path.TP;
+                currentPaths.push_back(path);
+            }
+            Abass.push_back(currentPaths);
+        }
+        file.close();
+    } else {
+        cout << "Unable to open the file." << endl;
+    }
+// Input Test index number please
+        const vector<Path>& pathVector = Abass[6];
+        for (const Path& path : pathVector) {
+            cout << "Start = " << path.Start << ", End = " << path.End
+                 << ", Length = " << path.Length << ", TP = " << path.TP << endl;
+        }
+        cout << "-----" << endl;
+    }
+};
+*/
