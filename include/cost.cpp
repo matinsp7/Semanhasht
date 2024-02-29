a3b00a47245932ef49e473ef29a5b3c532c2cfad
M@tin <matinparian@gmail.com.com>
2024-02-29 14:53:40 +0330

    Added Cost class

---
+++ include/cost.cpp
@@ -0,0 +1,7 @@
+#include "cost.h"
+
+Cost::Cost() {
+    taxi_cost = 2250;
+    subway_cost = 3267;
+    taxi_cost = 6000;
+}
---
+++ include/cost.h
@@ -0,0 +1,15 @@
+#ifndef COST_H
+#define COST_H
+
+class Cost
+{
+public:
+    Cost();
+
+private:
+    int bus_cost;
+    int subway_cost;
+    int taxi_cost;
+};
+
+#endif // COST_H
